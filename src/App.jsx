dd24f8c9c432b93686dc457beea1b017cb9a6958
sheyla astorga <sheylaastorga1998@gmail.com>
2024-01-25 10:05:21 -0300

    agregue el contenedor de la lista de tareas y le agregue estilo al body

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container className="my-4 text-center border border-3 w-50">
+        <h1>Bienvenido</h1>
+      </Container>
+    </>
+  );
 }
 
 export default App;
